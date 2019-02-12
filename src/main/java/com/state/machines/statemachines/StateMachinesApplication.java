70a777b4e3db4e3889b071442d848443aad0cbd7
vsaev <vlado_l_s@yahoo.it>
2019-02-13 01:10:04 +0200

    State machines projects

---
+++ src/main/java/com/state/machines/statemachines/StateMachinesApplication.java
@@ -0,0 +1,14 @@
+package com.state.machines.statemachines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateMachinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StateMachinesApplication.class, args);
+    }
+
+}
+
